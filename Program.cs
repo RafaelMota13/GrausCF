4f673d7e6f4a9d756495bb05dbf62b99d7009015
Rafael <faelmota7@gmail.com>
2022-09-17 10:21:16 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double ce, fa;
+Console.Write("Digite a temperatura (C°): ");
+ce = Convert.ToDouble(Console.ReadLine());
+
+fa = ce * 1.8 + 32;
+
+Console.WriteLine($"\n{ce} C° equivalem a {fa} F°\n");
